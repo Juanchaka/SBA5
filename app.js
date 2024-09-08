5fa485ff0dc52871a59c9022befc6abbe65dce94
Juanchaka <162772765+Juanchaka@users.noreply.github.com>
2024-09-08 02:46:51 -0500

    added '/' path to make sure there is ana appropriate response to localhost:3000/ default path

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const isAuthenticated = require('./middleware/auth');
 const postRoutes = require('./routes/postRoutes');
 const userRoutes = require('./routes/userRoutes');
 const commentRoutes = require('./routes/commentRoutes');
+const Post = require("./models/Post");
 
 const app = express();
 
@@ -20,14 +21,20 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
 
-mongoose.connect("mongodb://localhost:27017/blog-platform", {
-    useNewURLParser: true,
-    useUnifiedTopology: true
-}).then(() => console.log("MongoDB connected")).catch(err => console.err(err));
+mongoose.connect("mongodb://localhost:27017/blog-platform").then(() => console.log("MongoDB connected")).catch(err => console.error(err));
 
-app.get("/", (req, res) => {
-    res.send("Welcome to SBA5");
-});
+// app.get("/", (req, res) => {
+//     res.send("Welcome to SBA5");
+// });
+app.get('/', async (req, res) => {
+    try {
+      const posts = await Post.find().populate('author', 'username').exec();
+      res.render('index', { title: 'Home', posts });
+    } catch (err) {
+      res.status(500).send(err.message);
+    }
+  });
+
 
 app.use('/posts', postRoutes);
 app.use('/users', userRoutes);
