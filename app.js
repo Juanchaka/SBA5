42692386529d53cc6489a666fef2778365d3183e
Juanchaka <162772765+Juanchaka@users.noreply.github.com>
2024-09-08 02:55:40 -0500

    removed mongodb as it is not meeting the requirement for custom API

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const path = require("path");
 const logger = require('./middleware/logger');
@@ -21,11 +21,11 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
 
-mongoose.connect("mongodb://localhost:27017/blog-platform").then(() => console.log("MongoDB connected")).catch(err => console.error(err));
+// mongoose.connect("mongodb://localhost:27017/blog-platform").then(() => console.log("MongoDB connected")).catch(err => console.error(err));
 
-// app.get("/", (req, res) => {
-//     res.send("Welcome to SBA5");
-// });
+// // app.get("/", (req, res) => {
+// //     res.send("Welcome to SBA5");
+// // });
 app.get('/', async (req, res) => {
     try {
       const posts = await Post.find().populate('author', 'username').exec();
