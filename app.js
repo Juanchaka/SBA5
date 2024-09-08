d54f1aba7c795c7d01e8fc314f361e318f5dbded
Juanchaka <162772765+Juanchaka@users.noreply.github.com>
2024-09-08 00:09:12 -0500

    Added some middleware, the view engine, mongodb, home route, and started the express server

--- app.js
+++ app.js
@@ -7,3 +7,25 @@ const app = express();
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+
+app.set("view engine", "ejs");
+app.set("view", path.join(_dirname, "views"));
+
+mongoose.connect("mongodb://localhost:27017/blog-platform", {
+    useNewURLParser: true,
+    useUnifiedTopology: true
+}).then(() => console.log("MongoDB connected")).catch(err => console.err(err));
+
+app.get("/", (req, res) => {
+    res.send("Welcome to SBA5");
+});
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send("Something's wrong!");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+})
