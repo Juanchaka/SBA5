39fff26672c251a53c22de8e305d55bdafd30215
Juanchaka <162772765+Juanchaka@users.noreply.github.com>
2024-09-08 01:38:09 -0500

    added postcontroller for CRUD post functions

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,25 +1,54 @@
 const Post = require('../models/Post');
+const User = require("../models/User");
 const Comment = require('../models/Comment');
 
 exports.getAllPosts = async (req, res) => {
   try {
-    const posts = await Post.find().populate('author');
+    const posts = await Post.find().populate("authoer", "username").exec();
     res.render('index', { posts });
   } catch (err) {
     res.status(500).send(err.message);
-  }
+  };
 };
 
 exports.getPostById = async (req, res) => {
   try {
-    const post = await Post.findById(req.params.id).populate('author');
-    const comments = await Comment.find({ post: post._id }).populate('author');
-    res.render('post', { post, comments });
+    const post = await Post.findById(req.params.id).populate("author", "usernmae").exec();
+    const comments = await Comment.find({ post: req.params.id }).populate("author", "username").exec();
+    res.render('post', {post, comments});
   } catch (err) {
     res.status(500).send(err.message);
-  }
+  };
 };
 
 exports.createPost = async (req, res) => {
-  try {
-    const { title, content } = req
+    try {
+        const newPost = new Post({
+            title: req.body.title,
+            content: req.body.content,
+            author: req.body.author
+        });
+        await newPost.save();
+        res.redirect("/posts");
+    } catch (err) {
+        res.status(500).send(err.message)
+    };
+};
+
+exports.updatePost = async () => {
+    try {
+        const updatePost = await Post.findByIdAndUpdate(req.params.id, req.body, {new: true}).exec();
+        res.redirect("/posts/${updatedPost.id}");
+    } catch (err) {
+        res.status(500).send(err.message);
+    };
+};
+
+exports.deletePost = async () => {
+    try {
+        await Post.findByIdAndDelete(req.params.id).exec();
+        res.redirect("/posts");
+    } catch(err) {
+        res.status(500).send(err.message);
+    };
+};
