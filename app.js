addc7213241ae508b2fffb5478e42f9f963f0b0c
Juanchaka <162772765+Juanchaka@users.noreply.github.com>
2024-09-08 00:23:37 -0500

    was using some deprecated mongoose methods, commented out

--- app.js
+++ app.js
@@ -9,12 +9,12 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 app.set("view engine", "ejs");
-app.set("view", path.join(_dirname, "views"));
+app.set("view", path.join(__dirname, "views"));
 
-mongoose.connect("mongodb://localhost:27017/blog-platform", {
-    useNewURLParser: true,
-    useUnifiedTopology: true
-}).then(() => console.log("MongoDB connected")).catch(err => console.err(err));
+// mongoose.connect("mongodb://localhost:27017/blog-platform", {
+//     useNewURLParser: true,
+//     useUnifiedTopology: true
+// }).then(() => console.log("MongoDB connected")).catch(err => console.err(err));
 
 app.get("/", (req, res) => {
     res.send("Welcome to SBA5");
@@ -25,7 +25,7 @@ app.use((err, req, res, next) => {
     res.status(500).send("Something's wrong!");
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = /*process.env.PORT || */3000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 })
