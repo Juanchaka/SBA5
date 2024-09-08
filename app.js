ece80afe8aad83412b44de459bf903d41165f320
Juanchaka <162772765+Juanchaka@users.noreply.github.com>
2024-09-08 02:29:11 -0500

    added all middleware to app.js and refactored to show 'welcome to SBA5 message.

--- app.js
+++ app.js
@@ -1,43 +1,41 @@
 const express = require("express");
-// const mongoose = require("mongoose");
-// const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 const path = require("path");
-const { title } = require("process");
+const logger = require('./middleware/logger');
+const errorHandler = require('./middleware/errorHandler');
+const isAuthenticated = require('./middleware/auth');
 const postRoutes = require('./routes/postRoutes');
 const userRoutes = require('./routes/userRoutes');
 const commentRoutes = require('./routes/commentRoutes');
 
 const app = express();
 
-// app.use(bodyParser.urlencoded({extended: true}));
-// app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(logger);
+
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
 
-// mongoose.connect("mongodb://localhost:27017/blog-platform", {
-//     useNewURLParser: true,
-//     useUnifiedTopology: true
-// }).then(() => console.log("MongoDB connected")).catch(err => console.err(err));
-
-// app.get("/", (req, res) => {
-//     res.send("Welcome to SBA5");
-// });
+mongoose.connect("mongodb://localhost:27017/blog-platform", {
+    useNewURLParser: true,
+    useUnifiedTopology: true
+}).then(() => console.log("MongoDB connected")).catch(err => console.err(err));
 
 app.get("/", (req, res) => {
-    res.render("index", {title: "SBA5"});
+    res.send("Welcome to SBA5");
 });
 
 app.use('/posts', postRoutes);
 app.use('/users', userRoutes);
 app.use('/comments', commentRoutes);
-app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).send("Something's wrong!");
-});
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
-})
+});
