55a1a5a91125c9e6e72b2bb304d01054189504fd
Juanchaka <162772765+Juanchaka@users.noreply.github.com>
2024-09-08 12:12:25 -0500

    adding a bit of css

--- app.js
+++ app.js
@@ -52,37 +52,3 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-
-
-// const express = require('express');
-// const bodyParser = require('body-parser');
-// const path = require('path');
-// const logger = require('./middleware/logger');
-// const errorHandler = require('./middleware/errorHandler');
-// const requestTime = require('./middleware/requestTime');
-// const postRoutes = require('./routes/postRoutes');
-// const userRoutes = require('./routes/userRoutes');
-// const commentRoutes = require('./routes/commentRoutes');
-
-// const app = express();
-
-// app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(bodyParser.json());
-// app.use(logger);
-// app.use(requestTime);
-
-// app.set('view engine', 'ejs');
-// app.set('views', path.join(__dirname, 'views'));
-
-// app.use(express.static(path.join(__dirname, 'public')));
-
-// app.use('/posts', postRoutes);
-// app.use('/users', userRoutes);
-// app.use('/comments', commentRoutes);
-
-// app.use(errorHandler);
-
-// const PORT = process.env.PORT || 3000;
-// app.listen(PORT, () => {
-//   console.log(`Server running on port ${PORT}`);
-// });
