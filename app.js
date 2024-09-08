ab1aa2073f8dea313029f962fc6864ca5765c83b
Juanchaka <162772765+Juanchaka@users.noreply.github.com>
2024-09-08 00:46:57 -0500

    edited middleware to apply styles from correct public path

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
 const express = require("express");
-const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
+// const mongoose = require("mongoose");
+// const bodyParser = require("body-parser");
 const path = require("path");
 const { title } = require("process");
 
 const app = express();
 
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({extended: true}));
+// app.use(bodyParser.json());
 
 app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "/views"));
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
 
 // mongoose.connect("mongodb://localhost:27017/blog-platform", {
 //     useNewURLParser: true,
