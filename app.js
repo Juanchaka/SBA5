4bbd14a7a42ddf461529fc86ff49f688aa6abac1
Juanchaka <162772765+Juanchaka@users.noreply.github.com>
2024-09-08 00:36:53 -0500

    edited app.set to correct 'views' and path to load the view for default path successfully

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const path = require("path");
+const { title } = require("process");
 
 const app = express();
 
@@ -9,15 +10,19 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 app.set("view engine", "ejs");
-app.set("view", path.join(__dirname, "views"));
+app.set("views", path.join(__dirname, "/views"));
 
 // mongoose.connect("mongodb://localhost:27017/blog-platform", {
 //     useNewURLParser: true,
 //     useUnifiedTopology: true
 // }).then(() => console.log("MongoDB connected")).catch(err => console.err(err));
 
+// app.get("/", (req, res) => {
+//     res.send("Welcome to SBA5");
+// });
+
 app.get("/", (req, res) => {
-    res.send("Welcome to SBA5");
+    res.render("index", {title: "SBA5"});
 });
 
 app.use((err, req, res, next) => {
@@ -25,7 +30,7 @@ app.use((err, req, res, next) => {
     res.status(500).send("Something's wrong!");
 });
 
-const PORT = /*process.env.PORT || */3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 })
